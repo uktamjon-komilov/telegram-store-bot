defd8885eceaf41a0811d1f7e422b618e051bfd4
Uktamjon <uktamforjob@gmail.com>
2021-06-30 20:01:46 +0500

    postgres settings are added

--- main/settings.py
+++ main/settings.py
@@ -66,12 +66,24 @@ AUTH_USER_MODEL = 'app.User'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'hamidulloh',
+            'USER': 'postgres',
+            'PASSWORD': 'hamidulloh',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 
 # DATABASES = {
 #     'default': {
